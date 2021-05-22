7a05fdc4aee344ec8c5b79bfd02ed1d871833987
Crack9527 <27617170399@qq.com>
2021-05-22 10:13:05 +0800

    add:增加post请求json方法

--- common/util/HttpUtil.go
+++ common/util/HttpUtil.go
@@ -1,10 +1,12 @@
 package util
 
 import (
+	"bytes"
 	"github.com/crack007/api-base/common/config"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -22,9 +24,61 @@ func (h *HttpException) Error() string {
 	return h.error
 }
 
-func Get(url string) (string, error) {
+func Get(requestUrl string) (string, error) {
 	client := GetHttpClient()
-	response, err := client.Get(url)
+	response, err := client.Get(requestUrl)
+	if err != nil {
+		log.Println("request err:", err)
+		return "", &HttpException{
+			error: err.Error(),
+		}
+	}
+	defer func() {
+		if response != nil && response.Body != nil {
+			response.Body.Close()
+		}
+	}()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println("response read err:", err)
+		return "", &HttpException{
+			error: err.Error(),
+		}
+	}
+	result := string(body)
+	log.Println(result)
+	return result, nil
+}
+
+func PostForm(requestUrl string, data url.Values) (string, error) {
+	response, err := http.PostForm(requestUrl, data)
+	if err != nil {
+		log.Println("request err:", err)
+		return "", &HttpException{
+			error: err.Error(),
+		}
+	}
+	defer func() {
+		if response != nil && response.Body != nil {
+			response.Body.Close()
+		}
+	}()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println("response read err:", err)
+		return "", &HttpException{
+			error: err.Error(),
+		}
+	}
+	result := string(body)
+	log.Println(result)
+	return result, nil
+}
+
+func PostJson(url string, data string) (string, error) {
+	response, err := http.Post(url,
+		"application/json",
+		bytes.NewBuffer([]byte(data)))
 	if err != nil {
 		log.Println("request err:", err)
 		return "", &HttpException{
